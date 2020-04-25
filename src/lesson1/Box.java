86fcf2a8f7f1568e526758807035124ec9dd4dd7
indivioM <indivio@yandex.ru>
2020-04-25 19:08:36 +0300

    lesson 1 HW

---
+++ src/lesson1/Box.java
@@ -0,0 +1,31 @@
+package lesson1;
+
+import java.util.ArrayList;
+
+public class Box<T extends Fruit> {
+    ArrayList <T> inbox  = new ArrayList <T>();;
+    float weight=0;
+
+    public float getWeight() {
+        return this.weight*this.inbox.size();
+    }
+
+    /*public Box(){
+        inbox = new ArrayList <T>();
+    }*/
+    public void add(T obj){
+        this.inbox.add(obj);
+        this.weight=obj.weight;
+    }
+
+    public boolean compare(Box<? extends Fruit> obj) {
+        return Math.abs(this.getWeight()-obj.getWeight())<0.0001;
+    }
+    public boolean pourFrom(Box<T> obj) {
+
+        for(int i=obj.inbox.size();i>0;i--){
+            this.inbox.add((T) obj.inbox.remove(i-1));
+        }
+        return true;
+    }
+}
