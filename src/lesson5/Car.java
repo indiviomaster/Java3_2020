425e024e745aca720514f31609907078423bb451
indivioM <indivio@yandex.ru>
2020-05-09 18:32:15 +0300

    lesson 5 Race

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -1,9 +1,15 @@
 package lesson5;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static AtomicBoolean win = new AtomicBoolean(true);
     static {
         CARS_COUNT = 0;
+
     }
     private Race race;
     private int speed;
@@ -14,11 +20,17 @@ public class Car implements Runnable {
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    CyclicBarrier cb;
+    CountDownLatch cdlStart,cdlStop;
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdlStart,CountDownLatch cdlStop) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.cdlStart = cdlStart;
+        this.cdlStop = cdlStop;
+
     }
     @Override
     public void run() {
@@ -26,11 +38,19 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdlStart.countDown();
+            cb.await();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if(win.getAndSet(false)){
+            System.out.println(this.name + "WIN!!!");
+        }
+
+        cdlStop.countDown();
     }
 }
--- src/lesson5/Tunnel.java
+++ src/lesson5/Tunnel.java
@@ -1,21 +1,28 @@
 package lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+
+    private static Semaphore smf;
+    public Tunnel(Semaphore smf) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        this.smf = smf;
     }
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smf.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                smf.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
